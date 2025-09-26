24005a777919df91aaeb8fbc5e2b8c0a0a55c957
vitroid <vitroid@gmail.com>
2025-09-26 18:54:00 +0900

    Update to Yaplot v4.2 with trackpad gesture support
    - Add pinch-to-zoom gesture (equivalent to * and / keys)
    - Add two-finger swipe up/down for field of view control (equivalent to [ and ] keys)
    - Update SHA256 hash for v4.2 tarball
    - Improve test to avoid potential segfault with help flag

--- yaplot.rb
+++ yaplot.rb
@@ -2,26 +2,24 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Yaplot < Formula
-  desc "Yet another plotter in 3D - easy 3D modeller and animator for visualizing simulation results"
+  desc "Yet another plotter in 3D - easy 3D modeller and animator with trackpad gesture support"
   homepage "https://github.com/vitroid/Yaplot"
-  url "https://github.com/vitroid/Yaplot/archive/v4.1.3.tar.gz"
-  sha256 "ecbc312cbcc0ac55a30444f3f8bf52faa9fa159409a6295c786fa528c72397fe"
+  url "https://github.com/vitroid/Yaplot/archive/v4.2.tar.gz"
+  sha256 "2d07b552bc8510e366fc04fbcd5b09191986bd78c21d6edfc3bfa3b0a425594f"
   license "MIT"
 
   depends_on "gtk+3"
   depends_on "pkg-config" => :build
   depends_on "libpng"
 
-  # No pre-built bottles available for this third-party formula
-
   def install
     # Generate common.h from template
     system "make", "gen_common", "PKGDATADIR=#{share}/yaplot", "BINDIR=#{bin}"
     
-    # Build the program (but don't use the install target which may have issues)
-    system "make", "yaplot", "PKGDATADIR=#{share}/yaplot", "BINDIR=#{bin}"
+    # Build the program
+    system "make", "PKGDATADIR=#{share}/yaplot", "BINDIR=#{bin}"
     
-    # Install manually
+    # Install manually since the Makefile's install target uses hardcoded paths
     bin.install "yaplot"
     (share/"yaplot").install "yaplot.col", "help.yap"
   end
